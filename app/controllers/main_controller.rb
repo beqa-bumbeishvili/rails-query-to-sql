5ab9ba1622253c44457422cbe886aa6f07c1c0e4
beqa-bumbeishvili <bumbeishvili.beqa@gmail.com>
2017-07-14 17:59:37 +0400

    changes

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,21 +5,32 @@ class MainController < ApplicationController
   end
 
   def convert
-    query_string = params['query']
-    @errors = find_errors(query_string)
-    inside_brackets = query_string.scan(/\(([^\)]+)\)/)
-    inside_brackets.each { |substring| query_string.slice! substring.first }
-    commands_array =  query_string.gsub('()', '').split('.')
-    sql_structure_hash = {
-      main_table: commands_array.first
-    }
-    1.upto(commands_array.length) do |i|
-      sql_structure_hash["#{commands_array[i]}"] ||= []
-      sql_structure_hash["#{commands_array[i]}"] << inside_brackets[i-1]
-      sql_structure_hash["#{commands_array[i]}"].flatten!
+    if params['query'].present?
+      query_string = params['query']
+      @errors = find_errors(query_string)
+      query_commands = %w(select where find distinct group having includes joins order preload )
+      sql_structure_hash = {
+          main_table: query_string.split('.').first
+      }
+
+      query_commands.each do |command|
+        if query_string.include? command
+          command_index = query_string.index("#{command}") + "#{command}".length
+          sql_structure_hash[command] = query_string[command_index..(query_string[command_index..-1]).index(')') + command_index]
+        end
+      end
+
+      commands_array =  query_string.gsub('()', '').split('.')
+
+      1.upto(commands_array.length) do |i|
+        sql_structure_hash["#{commands_array[i]}"] ||= []
+        sql_structure_hash["#{commands_array[i]}"] << inside_brackets[i-1]
+        sql_structure_hash["#{commands_array[i]}"].flatten!
+      end
+
+      @convert = ''
     end
 
-    @convert = ''
     render :convert
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'main/index'
   get 'main/convert'
+  root 'main#convert'
 end
